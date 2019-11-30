f3cca1fcd9dc815b0959e90ae667c003ff1f1b82
kboban <kboban@foi.hr>
2019-11-30 20:16:47 +0100

    Implementirana MyDatabase klasa - singleton

--- database/src/main/java/hr/foi/air/database/MyDatabase.kt
+++ database/src/main/java/hr/foi/air/database/MyDatabase.kt
@@ -1,11 +1,29 @@
 package hr.foi.air.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import hr.foi.air.database.entities.Discount
 import hr.foi.air.database.entities.Store
 import hr.foi.air.database.views.DiscountDetails
+import java.security.AccessControlContext
 
 @Database(version = 1, entities = [Store::class, Discount::class], views = [DiscountDetails::class], exportSchema = false)
 public abstract class MyDatabase : RoomDatabase() {
+    companion object{
+        private var INSTANCE:MyDatabase? = null
+
+        fun getInstance(context: Context): MyDatabase? {
+            if(INSTANCE == null){
+                synchronized(MyDatabase::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, MyDatabase::class.java, "main" ).allowMainThreadQueries().build()
+                }
+            }
+            return INSTANCE
+        }
+        fun destroyInstance(){
+            INSTANCE = null
+        }
+    }
 }
