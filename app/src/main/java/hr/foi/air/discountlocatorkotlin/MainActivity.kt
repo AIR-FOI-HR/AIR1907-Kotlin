505f735ac9c4800caa90e5d9484a076de000916b
kboban <kboban@foi.hr>
2019-11-30 20:20:49 +0100

    Dohvaćena instanca klase MyDatabase u activityju

--- app/src/main/java/hr/foi/air/discountlocatorkotlin/MainActivity.kt
+++ app/src/main/java/hr/foi/air/discountlocatorkotlin/MainActivity.kt
@@ -2,11 +2,14 @@ package hr.foi.air.discountlocatorkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hr.foi.air.database.MyDatabase
 
 class MainActivity : AppCompatActivity() {
 
+    var database:MyDatabase? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        database = MyDatabase.getInstance(this)
     }
 }
