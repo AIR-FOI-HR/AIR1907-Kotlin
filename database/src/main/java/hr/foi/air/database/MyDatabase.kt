f620a6bea8223da51668741e56dab08ec5a22ad3
kboban <kboban@foi.hr>
2019-11-30 17:15:44 +0100

    Kreirana klasa MyDatabase koja nasljeđuje klasu RoomDatabase - obzirom da nismo kreirali Entities dobit ćemo grešku pri Build-u

---
+++ database/src/main/java/hr/foi/air/database/MyDatabase.kt
@@ -0,0 +1,8 @@
+package hr.foi.air.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(version = 1, entities = {}, views = {}, exportSchema = false)
+public abstract class MyDatabase : RoomDatabase() {
+}
