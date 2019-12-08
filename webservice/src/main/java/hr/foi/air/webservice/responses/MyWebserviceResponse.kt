d2cf9d42c46a637d5ca61ce63ba978b143d2fa08
matejloncar <matejloncar97@gmail.com>
2019-12-08 15:15:31 +0100

    Dodani webservice interface, response i caller.

---
+++ webservice/src/main/java/hr/foi/air/webservice/responses/MyWebserviceResponse.kt
@@ -0,0 +1,9 @@
+package hr.foi.air.webservice.responses
+
+class MyWebserviceResponse {
+
+    val responseId: Int? = null
+    val responseText: String? = ""
+    val timeStamp: Long? = null
+    val items: String?= ""
+}
