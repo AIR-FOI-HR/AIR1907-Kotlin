ada731da4ce0ec36c96977c299bc45be433c19c5
kboban <kboban@foi.hr>
2019-12-10 18:37:38 +0100

    Popravak MyDatabase klase

--- database/src/main/java/hr/foi/air/database/MyDatabase.kt
+++ database/src/main/java/hr/foi/air/database/MyDatabase.kt
@@ -7,17 +7,16 @@ import androidx.room.RoomDatabase
 import hr.foi.air.database.entities.Discount
 import hr.foi.air.database.entities.Store
 import hr.foi.air.database.views.DiscountDetails
-import java.security.AccessControlContext
 
 @Database(version = 1, entities = [Store::class, Discount::class], views = [DiscountDetails::class], exportSchema = false)
 public abstract class MyDatabase : RoomDatabase() {
     companion object{
         private var INSTANCE:MyDatabase? = null
 
-        fun getInstance(context: Context): MyDatabase? {
+        fun getInstance(context: Context?): MyDatabase? {
             if(INSTANCE == null){
                 synchronized(MyDatabase::class){
-                    INSTANCE = Room.databaseBuilder(context.applicationContext, MyDatabase::class.java, "main" ).allowMainThreadQueries().build()
+                    INSTANCE = Room.databaseBuilder(context?.applicationContext!!, MyDatabase::class.java, "main" ).allowMainThreadQueries().build()
                 }
             }
             return INSTANCE
