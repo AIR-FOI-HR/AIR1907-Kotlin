041f8c5ade150597ba2772392d066ca96398c059
matejloncar <matejloncar97@gmail.com>
2019-12-12 12:50:18 +0100

    Redizajniran main activity xml i obrisan nepotreban gumb

--- app/src/main/java/hr/foi/air/discountlocatorkotlin/MainActivity.kt
+++ app/src/main/java/hr/foi/air/discountlocatorkotlin/MainActivity.kt
@@ -1,9 +1,6 @@
 package hr.foi.air.discountlocatorkotlin
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import hr.foi.air.core.DataLoadedListener
 import hr.foi.air.core.DataLoader
@@ -12,6 +9,7 @@ import hr.foi.air.database.entities.Discount
 import hr.foi.air.database.entities.Store
 import hr.foi.air.discountlocatorkotlin.loaders.WsDataLoader
 
+
 class MainActivity : AppCompatActivity(), DataLoadedListener {
 
     companion object{
@@ -19,35 +17,20 @@ class MainActivity : AppCompatActivity(), DataLoadedListener {
         public var database:MyDatabase? = null
     }
 
-    var mListView: ListView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         database = MyDatabase.getInstance(this)
-        val button = findViewById<Button>(R.id.test_button)
+    }
 
-        mListView = findViewById<ListView>(R.id.discount_list)
-
-        button.setOnClickListener{
-            val dataLoader: DataLoader = WsDataLoader()
-            dataLoader.loadData(this)
-        }
-
+    fun loadData() {
+        var dataLoader: DataLoader = WsDataLoader()
+        dataLoader.loadData(this)
     }
 
     @SuppressWarnings("unchecked")
     override fun onDataLoaded(stores: List<Store>?, discounts: List<Discount>?) {
-        val listItems: MutableList<String> = ArrayList<String>()
 
-        if (discounts != null) {
-            for (discount in discounts){
-                listItems.add(discount.name)
-            }
-        }
-
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems.toTypedArray())
-        mListView?.adapter = adapter
     }
 
 
