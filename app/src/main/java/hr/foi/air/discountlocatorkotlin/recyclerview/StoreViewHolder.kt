1310f2314e8034abbd0904dbe4522c5c8720d186
kboban <kboban@foi.hr>
2019-12-14 12:49:06 +0100

    Kreiran StoreViewHolder  - Kreirana klasa koja nasljeđuje ParentViewHolder  - Dodan konstruktor  - implementirana BindToData metoda  - korišten Picasso za dohvaćanje slike

---
+++ app/src/main/java/hr/foi/air/discountlocatorkotlin/recyclerview/StoreViewHolder.kt
@@ -0,0 +1,33 @@
+package hr.foi.air.discountlocatorkotlin.recyclerview
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.NonNull
+import com.bignerdranch.expandablerecyclerview.ParentViewHolder
+import com.squareup.picasso.Picasso
+import hr.foi.air.database.entities.Discount
+import hr.foi.air.database.entities.Store
+import hr.foi.air.discountlocatorkotlin.R
+import kotlinx.android.synthetic.main.store_list_item.view.*
+
+class StoreViewHolder : ParentViewHolder<ExpandableStoreItem, Discount> {
+
+
+    var storeName: TextView? = null
+    var storeDesc: TextView? = null
+    var storeImage: ImageView? = null
+
+    constructor(@NonNull itemView: View): super(itemView){
+        storeName = itemView.findViewById(R.id.store_name)
+        storeDesc = itemView.findViewById(R.id.store_desc)
+        storeImage = itemView.findViewById(R.id.store_image)
+    }
+
+    public fun BindToData(store:Store){
+        storeName?.text = store.name
+        storeDesc?.text = store.description
+        Picasso.with(itemView.context).load(store.imgUrl).into(storeImage)
+
+    }
+}
