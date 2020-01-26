740e918ad869e9575ab69b8f9ea7a93dbe0667aa
kboban <kboban@foi.hr>
2020-01-26 23:23:58 +0100

    Prikazana google karta

--- map/src/main/java/com/example/map/MapModule.kt
+++ map/src/main/java/com/example/map/MapModule.kt
@@ -5,17 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import hr.foi.air.map.R
 
-class MapModule : Fragment() {
+class MapModule : Fragment(), OnMapReadyCallback {
+
+    var map: GoogleMap? = null
+    var mapFragment: SupportMapFragment? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
