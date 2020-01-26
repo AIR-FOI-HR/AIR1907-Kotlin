fec4f09e897407d6a3d90f2f67b1e4cb6267e5c3
kboban <kboban@foi.hr>
2020-01-26 22:23:34 +0100

    Kretiran DataPresenter sucelje u core modulu koje moraju implementirati svaki modul za prikaz podataka

---
+++ core/src/main/java/hr/foi/air/core/DataPresenter.kt
@@ -0,0 +1,14 @@
+package hr.foi.air.core
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.fragment.app.Fragment
+import hr.foi.air.database.entities.Discount
+import hr.foi.air.database.entities.Store
+
+interface DataPresenter {
+    fun getIcon(context: Context): Drawable
+    fun getName(context: Context): String
+    fun getFragment(): Fragment
+    fun setData(stores: List<Store>, discounts: List<Discount>)
+}
